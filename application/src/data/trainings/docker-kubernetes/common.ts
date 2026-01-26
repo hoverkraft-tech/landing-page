2c133dc57f56e370228f70de19c53159ae582203
Emilien Escalle <emilien.escalle@escemi.com>
2026-01-26 21:15:58 +0100

    feat: improve images
    Signed-off-by: Emilien Escalle <emilien.escalle@escemi.com>

--- application/src/data/trainings/docker-kubernetes/common.ts
+++ application/src/data/trainings/docker-kubernetes/common.ts
@@ -1,4 +1,4 @@
 export const slug = 'docker-kubernetes';
 export const icon = 'tabler:container';
-export const image = '~/assets/images/programming-5.webp';
+export const image = '~/assets/images/pages/trainings/docker-kubernetes.png';
 export const instructor = 'Frédéric Léger';
--- application/src/data/trainings/platform-engineering/common.ts
+++ application/src/data/trainings/platform-engineering/common.ts
@@ -1,4 +1,4 @@
 export const slug = 'platform-engineering';
 export const icon = 'tabler:building-factory-2';
-export const image = '~/assets/images/programming-1.webp';
+export const image = '~/assets/images/pages/trainings/platform-engineering.png';
 export const instructor = 'Frédéric Léger';
